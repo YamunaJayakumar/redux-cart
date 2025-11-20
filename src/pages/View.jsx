f2eba21e54748ee7c4d2dae8d09375043f170ca8
YamunaJayakumar <yamunajayakumar789@gmail.com>
2025-11-20 11:30:51 +0530

    n

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -5,10 +5,10 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToWishlist } from '../redux/slices/wishlistSlice'
-
+import Swal from 'sweetalert2'
 function View() {
-  const userwishlist=useSelector(state=>state.wishlistReducer)
-  const dispatch=useDispatch()
+  const userwishlist = useSelector(state => state.wishlistReducer)
+  const dispatch = useDispatch()
   //get product id from url
   const { id } = useParams()
   console.log(id)
@@ -21,12 +21,18 @@ function View() {
       setProduct(allProducts.find((item) => item.id == id))
     }
   }, [])
-  const handleWishlist=()=>{
-    const existingProduct=userwishlist?.find(item=>item.id==id)
-    if(existingProduct){
-      alert("product already in wishlist")
+  const handleWishlist = () => {
+    const existingProduct = userwishlist?.find(item => item.id == id)
+    if (existingProduct) {
+      // alert("product already in wishlist")
+      Swal.fire({
+        title: 'sorry',
+        text: 'product already in wishlist...',
+        icon: 'error',
+        confirmButtonText: 'ok'
+      })
     }
-    else{
+    else {
       dispatch(addToWishlist(product))
     }
   }
@@ -56,18 +62,18 @@ function View() {
             }</p>
             <h4>Client reviews</h4>
             {
-              product?.reviews?.length>0?
-              product?.reviews?.map((item,index)=>(
-                <div key={index} className='my-3'>
-              <div className="row border shadow rounded w-100 p-3 my-2">
-                <h6><b>{item?.reviewerName}</b>:{item?.comment}</h6>
-                <h6>rating:{item?.rating}<FontAwesomeIcon icon={faStar} className='text-warning'></FontAwesomeIcon></h6>
-              </div>
-              
-              
-            </div>
-              )):
-              <div>No Client Reviews Are Available</div>
+              product?.reviews?.length > 0 ?
+                product?.reviews?.map((item, index) => (
+                  <div key={index} className='my-3'>
+                    <div className="row border shadow rounded w-100 p-3 my-2">
+                      <h6><b>{item?.reviewerName}</b>:{item?.comment}</h6>
+                      <h6>rating:{item?.rating}<FontAwesomeIcon icon={faStar} className='text-warning'></FontAwesomeIcon></h6>
+                    </div>
+
+
+                  </div>
+                )) :
+                <div>No Client Reviews Are Available</div>
             }
           </div>
         </div>
